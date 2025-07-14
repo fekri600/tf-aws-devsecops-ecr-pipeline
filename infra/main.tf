2fe44b9c423f41daf8945b98cd57707950ca0444
Fekri Saleh <fekri600@gmail.com>
2025-07-13 19:07:55 -0600

    run pipeline

--- infra/main.tf
+++ infra/main.tf
@@ -1,20 +1,14 @@
 module "oidc" {
   source = "./oidc"
-
-  # Pass remaining variables from bootstrap
-  iam_role_name        = var.iam_role_name
-  iam_policy_name      = var.iam_policy_name
-  oidc_url             = var.oidc_url
-  oidc_client_id_list  = var.oidc_client_id_list
-  oidc_thumbprint_list = var.oidc_thumbprint_list
-  policies_path = local.policies 
+  oidc = var.oidc
+  policies_path = local.policies  # Pass the path to the policies directory
 }
 
 
 module "ecr" {
   source           = "./ecr"
   project_settings = var.project_settings
-  policies_path = local.policies
+  policies_path = local.policies  # Pass the path to the policies directory
 }
 
 
